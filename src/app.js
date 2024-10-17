cef7ec1e319d85e1a9e8f1391c90451fd89678f3
BDiaz-L <79861209+BDiaz-L@users.noreply.github.com>
2024-10-17 01:28:04 +0000

    hacemos la creacion random de los numeros y letras

--- src/app.js
+++ src/app.js
@@ -8,4 +8,26 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  createRandomCard();
 };
+
+function createRandomCard() {
+  let randomNumber = generateCardNumber();
+  let randomSimbol = generateRandomSimbol();
+  console.log(`randomNumber: ${randomNumber}`);
+  console.log(`randomSimbol: ${randomSimbol}`);
+}
+
+function generateCardNumber() {
+  let arrayNumbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  return arrayNumbers[generateRandomNumber(arrayNumbers.length)];
+}
+
+function generateRandomSimbol() {
+  let arraySimbols = ["A", "J", "Q", "K"];
+  return arraySimbols[generateRandomNumber(arraySimbols.length)];
+}
+
+function generateRandomNumber(arraySize) {
+  return Math.floor(Math.random() * arraySize);
+}
